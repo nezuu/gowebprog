7fdea8fd44e0ac07ac5fea5027b853d4c95a17f1
比嘉将吾 <handball.7@i.softbak.jp>
2019-06-07 14:44:27 +0900

    リスト4.15

---
+++ ch04/list4.15/server.go
@@ -0,0 +1,39 @@
+// ブラウザへのクッキーの送信
+// 1.ブラウザのURLの鍵をクリック
+// 2.中身を確認
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	c1 := http.Cookie{
+		Name:     "first_cookie",
+		Value:    "Go Web Programming",
+		HttpOnly: true,
+	}
+	c2 := http.Cookie{
+		Name:     "second_cookie",
+		Value:    "Manning Publications Co",
+		HttpOnly: true,
+	}
+	http.SetCookie(w, &c1)
+	http.SetCookie(w, &c2)
+}
+
+func getCookie(w http.ResponseWriter, r *http.Request) {
+	h := r.Header["Cookie"]
+	fmt.Fprintln(w, h)
+}
+
+func main() {
+	sever := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/set_cookie", setCookie)
+	http.HandleFunc("/get_cookie", getCookie)
+	sever.ListenAndServe()
+}
