7e5b6b9955c0e9607d984827ea93b59035f0b140
比嘉将吾 <handball.7@i.softbak.jp>
2019-06-08 18:25:44 +0900

    リスト5.7

---
+++ ch05/list5.7/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl.html")
+	t.Execute(w, "hello")
+}
+
+func process2(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl2.html")
+	t.Execute(w, "hello")
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/process", process)
+	http.HandleFunc("/process2", process2)
+	server.ListenAndServe()
+}
